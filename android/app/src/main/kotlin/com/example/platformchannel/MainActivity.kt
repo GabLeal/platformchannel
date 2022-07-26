98517a3ae4ced51aa2d7b138cd19f82ee80ccd43
GabLeal <gabriel.leal@compasso.com.br>
2022-07-26 11:09:32 -0300

    feat: crinado canais de comunicação

--- android/app/src/main/kotlin/com/example/platformchannel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/platformchannel/MainActivity.kt
@@ -1,6 +1,60 @@
 package com.example.platformchannel
 
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "br.com.brightness"
+    private var temperature: Temperature =  Temperature()
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when(call.method){
+                "checkPermission" ->{
+                    val settingsCanWrite = Settings.System.canWrite(context)
+                    result.success(settingsCanWrite)
+                }
+                "openPermissionSettings"->{
+                    val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+                    context.startActivity(intent)
+                }
+                "changeBrightnessScreen"->{
+                    var brightnessValue = call.arguments!! as Int
+                    Settings.System.putInt(context.getContentResolver(),android.provider.Settings.System.SCREEN_BRIGHTNESS, brightnessValue);
+                }
+                "getBrightness"->{
+                    var value = Settings.System.getInt(context.getContentResolver(),android.provider.Settings.System.SCREEN_BRIGHTNESS)
+                    result.success(value)
+                }
+
+                else ->{
+                    result.notImplemented()
+                }
+            }
+        }
+
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, "sensor/temperature").setStreamHandler(temperature)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "sensor/temperature/activesensor").setMethodCallHandler { call, result ->
+            when(call.method){
+                "activeSensor" ->{
+                    temperature.start(this.context)
+                    result.success(true)
+                }
+                else ->{
+                    result.notImplemented()
+                }
+            }
+        }
+
+    }
 }
